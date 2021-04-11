a0a3daf812accbe54a442457233bcbe950119b96
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-04-12 01:52:41 +0300

    Account Details List verification

--- user.py
+++ user.py
@@ -4,7 +4,7 @@ class User:
         Create a new User with both Username and Password
     '''
     user_list = []
-
+    @classmethod
     def __init__(self,username,passwordinfo):
 
         self.username = username        
@@ -20,9 +20,31 @@ class User:
         self.user_name = Username
         self.passwordinfo = passwordinfo if passwordinfo else Accounts.password_new()
 
-
+    
     def delete_user(self):
         '''
         delete_contact method deletes a saved account from the user_list
         '''
         User.user_list.remove(self)
+
+    
+    @classmethod
+    def display_user(cls):
+        '''
+        method that returns the contact list
+        '''
+        return cls.user_list
+
+class Accounts():
+        '''
+            This is a record of the account to be Created
+        '''
+    accounts_list = []
+
+    def __init__(self, accountname, username, passwordinfo):
+        '''
+        Used to verify the account list
+        '''
+        self.accountname = accountname
+        self.username = username
+        self.passwordinfo = passwordinfo
