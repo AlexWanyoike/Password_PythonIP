ced614000cb2d4eb0d18fcf64601dbc27a79cd8f
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-04-11 22:50:18 +0300

    Username and Passwordinfo Created

--- run.py
+++ run.py
@@ -1,1 +1,4 @@
-print("Hello world")
+import random
+from user import User
+
+
---
+++ user.py
@@ -0,0 +1,11 @@
+class User:
+    '''
+        Create a new User with both Username and Password
+    '''
+    user_list = []
+
+     def __init__(self,username,passwordinfo):
+
+        self.username = username        
+        self.passwordinfo = passwordinfo
+        
