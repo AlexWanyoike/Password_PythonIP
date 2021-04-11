840002f3d96e279d7c827eb8e537a9e9e8fabcb0
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-04-11 23:07:42 +0300

    Save UserDetails On User.py

--- run.py
+++ run.py
@@ -1,4 +1,27 @@
 import random
 from user import User
+import unittest # Importing the unittest module
+import pyperclip
+
+def create_user(username,passwordinfo):
+    '''
+        Lets Create a New User
+    '''
+    user = User(username,passwordinfo)
+    return(user)
+
+        
 
 
+def test_init(self):
+    '''
+    test_init test case to test if the object is initialized properly
+    '''
+
+    self.assertEqual(self.new_user.username,"James")
+    self.assertEqual(self.new_user.passwordinfo,"Muriuki")
+    
+
+
+if __name__ == '__main__':
+    unittest.main()
--- user.py
+++ user.py
@@ -1,11 +1,16 @@
+import pyperclip
 class User:
     '''
         Create a new User with both Username and Password
     '''
     user_list = []
 
-     def __init__(self,username,passwordinfo):
+    def __init__(self,username,passwordinfo):
 
         self.username = username        
         self.passwordinfo = passwordinfo
+
+    def save_user(self):
+        
+        User.user_list.append(self)
         
