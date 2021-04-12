ac6e7181da1a10557913e6b9c40aa0eb60ba46b3
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-04-12 07:16:24 +0300

    If Elseif Function Created

--- run.py
+++ run.py
@@ -46,70 +46,29 @@ def password_new():
     return new_pass
 
 def main():
-    print("")
+    while True:
+        print('\n')
+        print("This is the lastest Password Input Locker")
+        print("Use these short codes : cc - create a new user, dc - display user, fc -find accounts, ex -exit the contact list ")
+
+        short_code = input().lower()
+        print('\n')
+        if short_code == 'cc'
+            username = input()
 
             while True:
-                    print("Use these short codes : cc - create a new contact, dc - display contacts, fc -find a contact, ex -exit the contact list ")
-
-                    short_code = input().lower()
-
-                    if short_code == 'cc':
-                            print("New Contact")
-                            print("-"*10)
-
-                            print ("First name ....")
-                            f_name = input()
-
-                            print("Last name ...")
-                            l_name = input()
-
-                            print("Phone number ...")
-                            p_number = input()
-
-                            print("Email address ...")
-                            e_address = input()
+                print("Create a New Username")
+                username = input()
+                while True:
+                    print("Would you like to create your own Password or generated?")
+                    passwordint = input().lower().strip()
+                    if passwordint === 'own'
+                        passwordint = input(Enter own Password)
+                    
+                    else passwordint === 'generate'
+                        passwordint = password()
 
 
-                            save_contacts(create_contact(f_name,l_name,p_number,e_address)) # create and save new contact.
-                            print ('\n')
-                            print(f"New Contact {f_name} {l_name} created")
-                            print ('\n')
-
-                    elif short_code == 'dc':
-
-                            if display_contacts():
-                                    print("Here is a list of all your contacts")
-                                    print('\n')
-
-                                    for contact in display_contacts():
-                                            print(f"{contact.first_name} {contact.last_name} .....{contact.phone_number}")
-
-                                    print('\n')
-                            else:
-                                    print('\n')
-                                    print("You dont seem to have any contacts saved yet")
-                                    print('\n')
-
-                    elif short_code == 'fc':
-
-                            print("Enter the number you want to search for")
-
-                            search_number = input()
-                            if check_existing_contacts(search_number):
-                                    search_contact = find_contact(search_number)
-                                    print(f"{search_contact.first_name} {search_contact.last_name}")
-                                    print('-' * 20)
-
-                                    print(f"Phone number.......{search_contact.phone_number}")
-                                    print(f"Email address.......{search_contact.email}")
-                            else:
-                                    print("That contact does not exist")
-
-                    elif short_code == "ex":
-                            print("Bye .......")
-                            break
-                    else:
-                            print("I really didn't get that. Please use the short codes")
 
 
 if __name__ == '__main__':
